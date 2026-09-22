8be90ba26e422f7c800622945b2235ce04cc52c6
Toletitus <mirefugio708@gmail.com>
2026-09-22 19:45:04 +0200

    Ejercicio de malla.c completado

--- Práctica 1/Ejercicios/malla.c
+++ Práctica 1/Ejercicios/malla.c
@@ -1,8 +1,20 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-int main(){
-
+int main(int argc, char const *argv[])
+{
+    int x,y;
+    x = atoi(argv[1]);
+    y = atoi(argv[2]);
     pid_t pid;
-    pid = fork();
+    for(int i=0; i<x; i++){
+        pid = fork();
+        if(pid == 0){
+            for(int j=0; j<y; j++){
+                printf("Soy el proceso %d y mi padre es %d\n", getpid(), getppid());
+            }
+        }
+    }
+
+    return 0;
 }
