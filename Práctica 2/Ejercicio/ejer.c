55037e3bb0793faa69500d3fa163cf7298f7f156
Toledo708 <mirefugio708@gmail.com>
2026-09-25 17:42:27 +0200

    Descripción de los cambios realizados en ejer.c

--- Práctica 2/Ejercicio/ejer.c
+++ Práctica 2/Ejercicio/ejer.c
@@ -20,63 +20,26 @@ pid_t Modulox(pid_t p){
     wait(NULL);
 }
 
-int* ModuloY(int x,pid_t p){
-
-    //Este modulo genera un cantidad de hijos x en el proceso p y lo almacena en un array
-    //Devuelve el puntero de esa array
-    int* array = (int*) malloc( x * sizeof(int));
-    if (array == NULL)
-    {
-        printf("Hemos comido polla\n");
-        printf("No sabemos donde coño esta el puntero con los procesos");
-    }
-    if (x > 0)
-    {
-       for (int i = 0; i < x; i++)
-        {
-            switch (fork())
-            {
-            case -1:
-                perror("Ha ocurrido algo durante el proceso de ejecución de xyz que ha fallado");
-                exit(1);
-                break;
-            case 0:
-                //Este sería el proceso hijo
-                //El proceso hijo va enviarle su pid al array que hemos creado
-                array[i] = getpid();
-                break;
-            default:
-                //El padre no tiene que hacer nada por lo que
-                break;
-            }
-            if(i != 2){
-                wait(NULL);
-            }else if (i == 2)
-            {
-                //Básicamente ejecuta el pstree para mostrar todos los procesos del programa
-                execlp("pstree -c", "pstree", "-c",NULL);
-                //En esta parte hay que empezar a matar a todos los hijos
-                aniquilarHijos();
-            }
-            
+void ModuloY(pid_t p){
+    for(int i = 0; i < 3 ; i++){
+        p = fork();
+        if(p == 0){
+            continue;
+        }else if(i == 2){
+            execlp("ver contenido", "pstree","-c",NULL);
+            aniquilarHijos();
         }
     }
-    
-
-    
-    
-
+    wait(NULL);
 }
-
-
 
 int main(int argc, char const *argv[])
 {
     pid_t pid;
     pid = Modulox(pid); // Esto sería A
     pid = Modulox(pid); // Esto sería B
-
-
+    ModuloY(pid); // Esto sería la creación de xyz
+    
 
     return 0;
 }
